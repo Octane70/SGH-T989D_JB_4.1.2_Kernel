20a442da49954db4590e3447c2d36265eb5dbd2d
Octane70 <robczepil@gmail.com>
2013-05-19 14:49:36 -0700

    Removed Smartass

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -410,9 +410,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
-extern struct cpufreq_governor cpufreq_gov_smartass;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass)
-#endif
 
 
 /*********************************************************************
